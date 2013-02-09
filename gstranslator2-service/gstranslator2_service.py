2152d10a7303b38057f7828a28ebbd48c61fdcb7
Radek Lewandowski <radlewand@o2.pl>
2013-02-09 16:57:58 +0100

    New version - 0.2

--- gstranslator2-service/gstranslator2_service.py
+++ gstranslator2-service/gstranslator2_service.py
@@ -22,6 +22,7 @@ import signal
 from gi.repository import Gio, Gtk, Gdk
 import dbus.service
 import gobject
+from copy import copy
 from dbus.mainloop.glib import DBusGMainLoop
 import textwrap
 #import pprint
@@ -30,44 +31,41 @@ class Gstranslator(dbus.service.Object):
     def __init__(self):
         bus_name = dbus.service.BusName('com.github.thof.gstranslator2', bus=dbus.SessionBus())
         dbus.service.Object.__init__(self, bus_name, '/com/github/thof/gstranslator2')
-        self.src_text = ''
         self.translation = ''
+        self.clipboardCurrent = ''
+        self.clipboardPrevious = ''
         self.trans_width = 80
         self.trans_expanded_width = 120
         self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_PRIMARY)
-        self.mode = False
-        self.lang_changed = False
         self.languages_list = []
         self.current_index = 0
+        self.states = []
+        self.states.append([0, 0, 0, 0, 0])
+        self.states.append([0, 0, 0, 0, 0])
+        self.setState()
+        self.clipboardPrevious = self.clipboard.wait_for_text()
         
         signal.signal(signal.SIGALRM, self.handler)
         self.load_config()
         
-    def translate_clipboard(self, expanded):
-        text = self.clipboard.wait_for_text()
-        text = self.fix_string(text)
-        if self.mode==expanded or self.lang_changed:
-            self.translate_text(text, expanded)
-            self.lang_changed = False
-        elif text!=self.src_text:
-            self.translate_text(text, expanded)
+    def getClipboard(self):
+        self.clipboardCurrent = self.clipboard.wait_for_text()
+        text = self.fix_string(self.clipboardCurrent)
+        return text
         
-    def translate_text(self, text, expanded):
-        self.src_text = text
-        self.translation = self.get_translation(self.src_text)
+    def translateText(self, text, expanded):
+        translation = self.getGoogleTranslation(text)
         if expanded:
-            self.translation = self.parse_json_full_translation(self.translation)
-            self.mode = False;
+            self.translation = self.parseJsonExpandedTranslation(translation)
         else:
-            self.translation = self.parse_json_translation(self.translation)
-            self.mode = True;
+            self.translation = self.parseJsonTranslation(translation)
         
-    def translate_custom(self, text):
-        translation = self.get_translation(text)
-        translation = self.parse_json_full_translation(translation)
+    def translateCustom(self, text):
+        translation = self.getGoogleTranslation(text)
+        translation = self.parseJsonExpandedTranslation(translation)
         return translation
     
-    def get_translation (self, text_trans):
+    def getGoogleTranslation(self, text_trans):
         headers = {'User-Agent' : 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
         text = urllib2.quote(text_trans.encode('utf8'))
         url = 'http://translate.google.com/translate_a/t?client=json&sl='+self.languages_list[self.current_index][0]+'&tl='+self.languages_list[self.current_index][1]+'&hl='+self.languages_list[self.current_index][1]+'&text='+text
@@ -81,12 +79,12 @@ class Gstranslator(dbus.service.Object):
         translation = response.read()
         return translation
     
-    def parse_json_translation(self, translation):
+    def parseJsonTranslation(self, translation):
         term_string = ''
         dump = json.loads(translation)
         #pprint.pprint(dump)
         dict_path = dump['sentences']
-        trans_string = self.parse_basic_translation(dict_path, self.trans_width)
+        trans_string = self.parseBasicTranslation(dict_path, self.trans_width)
         
         try:
             dict_path = dump['dict']
@@ -101,12 +99,12 @@ class Gstranslator(dbus.service.Object):
             term_string = ''
         return trans_string[:-2]
     
-    def parse_json_full_translation(self, translation):
+    def parseJsonExpandedTranslation(self, translation):
         term_string = ''
         dump = json.loads(translation)
         #pprint.pprint(dump)
         dict_path = dump['sentences']
-        trans_string = self.parse_basic_translation(dict_path, self.trans_expanded_width)
+        trans_string = self.parseBasicTranslation(dict_path, self.trans_expanded_width)
         
         try:
             dict_path = dump['dict']
@@ -127,7 +125,7 @@ class Gstranslator(dbus.service.Object):
             trans_string += "\n"
         return trans_string[:-2]
     
-    def parse_basic_translation(self, dict_path, width):
+    def parseBasicTranslation(self, dict_path, width):
         orig_string = ""
         trans_string = ""
         for d in dict_path:
@@ -140,60 +138,98 @@ class Gstranslator(dbus.service.Object):
         trans_string += "\n\n"
         return trans_string
     
-    def load_config(self):
-        schema = 'org.gnome.shell.extensions.gstranslator2'
-        settings = Gio.Settings.new(schema)
-        self.trans_width = int(settings.get_string('width'))
-        self.trans_full_width = int(settings.get_string('width-wide'))
-        self.load_languages(settings.get_string('items'))
-        
-    def load_languages(self, lang_list):   
-        langs = lang_list.split('|') 
-        for l in langs:
-            temp = l.split(';')
-            if int(temp[2]):
-                self.languages_list.append([temp[0], temp[1]])
-        #pprint.pprint(self.languages_list)
+    def getTranslation(self, state, text=None):
+        # default translation from clipboard
+        if state == 0:
+            self.translateText(self.getClipboard(), False)
+        # expanded translation from clipboard
+        elif state == 1:
+            self.translateText(self.getClipboard(), True)
+        # default translation from provided text
+        elif state == 2:
+            self.translateText(text, False)
+        # expanded translation from provided text
+        elif state == 3:
+            self.translateText(text, True)
+    
+    def openTranslation(self, state, checkClipboard=True, text=None):
+        if checkClipboard:
+            if self.compareClipboards(): # if contents of previous and current clipboard are the same then use previous translation
+                self.setState(state)
+                return "-222"        
+        self.getTranslation(state, text)
+        self.setState(state)
+        return self.translation
+            
+    
+    def closeTranslation(self, state):
+        if self.compareClipboards():    # if an old word is in the clipboard then close
+            self.setState()
+            return "-111"
+        else:                           # otherwise prepare new translation
+            self.getTranslation(state) 
+            return self.translation
+    
+    def checkCond(self, defPrev, expPrev, defCurr, expCurr):
+        if self.states[0][0]==defPrev and self.states[0][1]==expPrev and self.states[1][0]==defCurr and self.states[1][1]==expCurr:
+            return True
+        else:
+            return False
+    
+    @dbus.service.method('com.github.thof.gstranslator2')
+    def dbusGetTranslation(self, text, state):
+        #state = int(state_string)
+        # if languages were changed then prepare new translation in any case
+        if self.isOpen(4):
+            return self.openTranslation(state, False, text)
+        # conditions regarding translation from input field
+        elif state == 0 and (self.isOpen(2) or self.isOpen(3)):
+            return self.closeTranslation(state)
+        elif state == 2 or state == 3:
+            return self.openTranslation(state, False, text)
+        # conditions regarding translation from clipboard
+        elif (state==0 or state==1) and self.checkCond(0,0,0,0): # 1 and 2
+            return self.openTranslation(state)
+        elif (state==0 and self.checkCond(0, 0, 1, 0)) or (state==1 and self.checkCond(0, 0, 0, 1)): # 3 and 6
+            return self.closeTranslation(state)
+        elif (state==1 and self.checkCond(0, 0, 1, 0)) or (state==0 and self.checkCond(0, 0, 0, 1)): # 4 and 5
+            return self.openTranslation(state, False)
+        # 3rd level
+        elif state==0 and self.checkCond(1,0,0,0): # 1st
+            return self.openTranslation(state)
+        elif state==1 and self.checkCond(1,0,0,0): # 2nd
+            return self.openTranslation(state, False)
+        elif state==0 and self.checkCond(1,0,0,1): # 3rd
+            return self.openTranslation(state, False)
+        elif state==1 and self.checkCond(1,0,0,1): # 4th
+            return self.closeTranslation(state)
+        elif state==0 and self.checkCond(0,1,1,0): # 5th
+            return self.closeTranslation(state)
+        elif state==1 and self.checkCond(0,1,1,0): # 6th
+            return self.openTranslation(state, False)
+        elif state==0 and self.checkCond(0,1,0,0): # 7th
+            return self.openTranslation(state, False)
+        elif state==1 and self.checkCond(0,1,0,0): # 8th
+            return self.openTranslation(state)
         
     @dbus.service.method('com.github.thof.gstranslator2')
     def getCurrentLangs(self, text=''):
         return self.languages_list[self.current_index][0]+" -> "+self.languages_list[self.current_index][1]
-        
-    @dbus.service.method('com.github.thof.gstranslator2')
-    def nextLangs(self, text=''):
-        self.lang_changed = True
-        if(self.current_index == len(self.languages_list)-1):
-            self.current_index = 0
-        else:
-            self.current_index = self.current_index+1
-        #print self.languages_list[self.current_index][0]+" -> "+self.languages_list[self.current_index][1]
-        return self.languages_list[self.current_index][0]+" -> "+self.languages_list[self.current_index][1]
-            
-    @dbus.service.method('com.github.thof.gstranslator2')
-    def prevLangs(self, text=''):
-        self.lang_changed = True
-        if(self.current_index == 0):
-            self.current_index = len(self.languages_list)-1
-        else:
-            self.current_index = self.current_index-1
-        #print self.languages_list[self.current_index][0]+" -> "+self.languages_list[self.current_index][1]
-        return self.languages_list[self.current_index][0]+" -> "+self.languages_list[self.current_index][1]
-        
-    @dbus.service.method('com.github.thof.gstranslator2')
-    def dbusGetTranslation(self, text=''):
-        if not text:
-            self.translate_clipboard(False)
-        else:
-            self.translate_text(text, False)
-        return self.translation
     
     @dbus.service.method('com.github.thof.gstranslator2')
-    def dbusGetExpandedTranslation(self, text=''):
-        if not text:
-            self.translate_clipboard(True)
+    def changeLangs(self, direction):
+        self.setState(4)
+        if direction:
+            if(self.current_index == len(self.languages_list)-1):
+                self.current_index = 0
+            else:
+                self.current_index = self.current_index+1
         else:
-            self.translate_text(text, True)
-        return self.translation
+            if(self.current_index == 0):
+                self.current_index = len(self.languages_list) - 1
+            else:
+                self.current_index = self.current_index - 1
+        return self.languages_list[self.current_index][0]+" -> "+self.languages_list[self.current_index][1]
     
     def fix_string (self, text):
         text = text.strip()
@@ -215,6 +251,53 @@ class Gstranslator(dbus.service.Object):
     def handler (self, signum, frame):
         print 'Signal handler called with signal', signum
         raise IOError("Could not open url!")
+    
+    def compareClipboards(self):
+        self.clipboardCurrent = self.clipboard.wait_for_text()
+        if self.clipboardCurrent == None or self.clipboardCurrent == self.clipboardPrevious:
+            return True
+        else:
+            self.clipboardPrevious = self.clipboardCurrent
+            return False
+    
+    def setState (self, number=None):
+        self.states[0] = copy(self.states[1])
+        self.states[1][0] = 0 # translation from clipboard
+        self.states[1][1] = 0 # expanded translation from clipboard
+        self.states[1][2] = 0 # translation from input field
+        self.states[1][3] = 0 # expanded translation from input field
+        self.states[1][4] = 0 # change of languages
+        if number is not None:
+            self.states[1][number] = 1  
+        print "prev: ",self.states[0][0]," ",self.states[0][1]," ",self.states[0][2]," ",self.states[0][3]," ",self.states[0][4]
+        print "curr: ",self.states[1][0]," ",self.states[1][1]," ",self.states[1][2]," ",self.states[1][3]," ",self.states[1][4]
+        print "\n"
+            
+    def isOpen (self, state):
+        if self.states[1][state]:
+            return True
+        else:
+            return False
+        
+    def wasOpen (self, state):
+        if self.states[0][state]:
+            return True
+        else:
+            return False
+        
+    def load_config(self):
+        schema = 'org.gnome.shell.extensions.gstranslator2'
+        settings = Gio.Settings.new(schema)
+        self.trans_width = int(settings.get_string('width'))
+        self.trans_full_width = int(settings.get_string('width-wide'))
+        self.load_languages(settings.get_string('items'))
+        
+    def load_languages(self, lang_list):   
+        langs = lang_list.split('|') 
+        for l in langs:
+            temp = l.split(';')
+            if int(temp[2]):
+                self.languages_list.append([temp[0], temp[1]])
         
     def main(self):
         mainloop = gobject.MainLoop()
--- gstranslator2@thof.github.com/extension.js
+++ gstranslator2@thof.github.com/extension.js
@@ -35,34 +35,23 @@ const Lib = Extension.imports.lib;
 
 const schema = "org.gnome.shell.extensions.gstranslator2";
 
-let text=null, lang_text=null, settings = null, transBox=null, scrollBox=null, content=null;
-let clipboard = "";
-let open = false, extended = false, normal=false;
+let translationLabel=null, langsLabel, langsLabelMenu, settings = null, transBox=null, scrollBox=null, content=null;
+let prevTrans = "";
 let x=0, y=0, xoffset=0, yoffset=0, position = 0;
 let monitor = Main.layoutManager.primaryMonitor;
-let gstrans, searchEntry, langsLabel, autoCloseVal, height, mainBox, searchEntryText, button, dbusNotify;
+let gstrans, searchEntry, autoCloseVal, height, mainBox, searchEntryText, button, dbusNotify;
 
 const GstranslatorInterface = {
     name: 'com.github.thof.gstranslator2',
     methods: [
 	    {
             name: 'dbusGetTranslation',
-            inSignature: 's',
+            inSignature: 'si',
             outSignature: 's'
         },
         {
-            name: 'dbusGetExpandedTranslation',
-            inSignature: 's',
-            outSignature: 's'
-        },
-        {
-            name: 'nextLangs',
-            inSignature: 's',
-            outSignature: 's'
-        },
-        {
-            name: 'prevLangs',
-            inSignature: 's',
+            name: 'changeLangs',
+            inSignature: 'i',
             outSignature: 's'
         },
         {
@@ -75,70 +64,21 @@ const GstranslatorInterface = {
 
 const key_bindings = {
 	'get-translation': function() {
-		_getTrans();
+		_getTrans(0);
 	},
 	'get-expanded-translation': function() {
-		_getExpandedTrans();
+		_getTrans(1);
 	},
 	'next-langs': function() {
-		_nextLangs();
+		_changeLangs(1);
 	},
 	'prev-langs': function() {
-		_prevLangs();
+		_changeLangs(0);
 	},
 	'toggle-menu': function() {
 		_toggleMenu();
 	},
 };
-
-function _nextLangs() {
-    dbusNotify.nextLangsRemote('', _showLangInfo);
-}
-
-function _prevLangs() {
-    dbusNotify.prevLangsRemote('', _showLangInfo);
-}
-
-function _showLangInfo(title) {
-    if (!lang_text) {
-        lang_text = new St.Label({ style_class: 'lang-label', text: title });
-        Main.uiGroup.add_actor(lang_text);
-    }
-    else {
-        Main.uiGroup.remove_actor(lang_text);
-        lang_text = new St.Label({ style_class: 'lang-label', text: title });
-        Main.uiGroup.add_actor(lang_text);
-    }
-    
-    _updateLangsLabel(title);
-    _getPosition(lang_text);
-    lang_text.set_position(x, y);
-    
-    Tweener.addTween(lang_text, { time: 3, 
-                            onComplete: _hideInfo });
-}
-
-function _hideInfo() {
-    if(lang_text!=null) {
-        Main.uiGroup.remove_actor(lang_text);
-        lang_text = null;
-    }
-}
-
-function _openTrans() {
-    open = true;
-}
-
-function _hideTrans() {
-    if (transBox!=null && text!=null){
-        Main.uiGroup.remove_actor(transBox);
-        text = null;
-        transBox = null;
-        scrollBox = null;
-        content = null;
-        open = false;
-    }
-}
 
 function _exitTransBox(actor, event) {
     let symbol = event.get_key_symbol();
@@ -153,90 +93,49 @@ function _exitTransBoxButton(actor, event) {
     _hideTrans();
 }
 
-function _showTrans(title) {
-    clipboard = title;
-    
-    if (transBox==null) {
-        transBox = new St.BoxLayout({style_class: "main-dialog",
-		    vertical: true
-	    });
-	    Main.uiGroup.add_actor(transBox);
-	
-	    scrollBox = new St.ScrollView();
-	    transBox.add(scrollBox);
-	    
-	    content = new St.BoxLayout({vertical: true});
-	    scrollBox.add_actor(content);
-	    
-	    text = new St.Label({ style_class: 'main-label', text: title });
-        content.add(text);
+function _showTrans(translation) {
+        if (translation != null){
+	        translationLabel.set_text(translation);
+            content.add(translationLabel);
 
-        _getPosition(text);
-        transBox.set_x(x);
-        transBox.set_y(y);
-        if(text.height>height) {
-            transBox.set_height(height);
-            transBox.set_reactive(true);
-            transBox.connect('button-press-event', _exitTransBoxButton);
-            transBox.connect('key-release-event', _exitTransBox);
-            Main.pushModal(transBox);
+            _getPosition(translationLabel);
+            transBox.set_x(x);
+            transBox.set_y(y);
+            if(translationLabel.height>height) {
+                transBox.set_height(height);
+                transBox.set_reactive(true);
+                transBox.connect('button-press-event', _exitTransBoxButton);
+                transBox.connect('key-release-event', _exitTransBox);
+                Main.pushModal(transBox);
+            }
         }
-	    _openTrans();
-	}
 }
 
-function _getTransFromEntry(text) {
-    if (open) {
-        _hideTrans();
-        dbusNotify.dbusGetTranslationRemote(text, _asyncSetTrans2);
-    }
-    else {
-        dbusNotify.dbusGetTranslationRemote(text, _asyncSetTrans);
+function _hideTrans() {
+    if (content.contains(translationLabel)) {
+        content.remove_all_children();
     }
 }
 
-function _getExpandedTransFromEntry(text) {
-    if (open) {
-        _hideTrans();
-        dbusNotify.dbusGetExpandedTranslationRemote(text, _asyncSetTrans2);
-    }
-    else {
-        dbusNotify.dbusGetExpandedTranslationRemote(text, _asyncSetTrans);
-    }
+function _getTrans(state, toTrans) {
+    toTrans = toTrans || '';
+    dbusNotify.dbusGetTranslationRemote(toTrans, state, _asyncSetTrans);
 }
 
-function _getTrans() {
-    if (open) {
-        _hideTrans();
-        dbusNotify.dbusGetTranslationRemote('', _asyncSetTrans2);
-    }
-    else {
-        dbusNotify.dbusGetTranslationRemote('', _asyncSetTrans);
-    }
-}
-
-function _getExpandedTrans() {
-    if (open) {
-        _hideTrans();
-        dbusNotify.dbusGetExpandedTranslationRemote('', _asyncSetTrans2);
-    }
-    else {
-        dbusNotify.dbusGetExpandedTranslationRemote('', _asyncSetTrans);
-    }
-}
-
-function _asyncSetTrans(title) {
-    if (title==null) { 
-        _showTrans(clipboard);
-    }
-    else {
-        _showTrans(title);
-    }
-}
-
-function _asyncSetTrans2(title) {
-    if (title!=clipboard && title!=null) {
-        _showTrans(title);
+function _asyncSetTrans(translation) {
+    //global.log('***** closing translation');
+    _hideTrans();
+    
+    if (translation != '-111') {
+        //global.log('***** opening translation');
+        if (translation == '-222') {
+            // show previous translation
+            _showTrans(prevTrans);
+        }
+        else {
+            _showTrans(translation);
+            prevTrans = translation;
+        }
     }
 }
 
@@ -264,35 +163,34 @@ Gstranslator.prototype = {
         searchEntryText.connect('key-press-event', this._onKeyPress);
         searchEntryText.connect('key-release-event', this._onKeyRelease);
         
-        langsLabel = new St.Label({ style_class: 'lang-label', text: '' });
+        langsLabelMenu = new St.Label({ style_class: 'lang-label'});
         dbusNotify.getCurrentLangsRemote('', _updateLangsLabel);
         
         this.autoClose = new PopupMenu.PopupSwitchMenuItem("Autoclose", autoCloseVal);
         this.autoClose.connect('toggled', changeAutoClose);
         
         this.mainBox.add(searchEntry);
-        this.mainBox.add(langsLabel);
+        this.mainBox.add(langsLabelMenu);
 
         this.section.actor.add_actor(this.mainBox);
         this.menu.addMenuItem(this.autoClose);
         
-        Main.panel._menus.addMenu(this.menu);
+        Main.panel.menuManager.addMenu(this.menu);
         this.menu.connect('open-state-changed', this._onOpenStateToggled);
+        Main.panel.addToStatusArea("gstranslator", this, 10, "right");
     },
     
     _onKeyRelease: function(actor, event) {
         let emptyEntry = searchEntry.get_text() == "";
         if(emptyEntry) {
-                if (open) {
                     _hideTrans();
-                }
         }
     },
     
     _onKeyPress: function(actor, event) {
         let symbol = event.get_key_symbol();
         let state = event.get_state();
-        global.log("State "+state);
+        //global.log("State "+state);
         if(symbol == Clutter.KEY_Return) {
             this.textTrans = searchEntry.get_text().replace(/^\s+|\s+$/g, '');
             if(autoCloseVal) {
@@ -300,20 +198,20 @@ Gstranslator.prototype = {
             }
             if(this.textTrans != "") {
                 if(state & state.SHIFT_MASK == state.SHIFT_MASK) {
-                    _getExpandedTransFromEntry(this.textTrans);
+                    _getTrans(3, this.textTrans);
                     return true;
                 }
                 else {
-                    _getTransFromEntry(this.textTrans);
+                    _getTrans(2, this.textTrans);
                 }
             }
         }
         else if(symbol == Clutter.KEY_Right && state == Clutter.ModifierType.CONTROL_MASK) {
-            _nextLangs();
+            _changeLangs(1);
             return true;
         }
         else if(symbol == Clutter.KEY_Left && state == Clutter.ModifierType.CONTROL_MASK) {
-            _prevLangs();
+            _changeLangs(0);
             return true;
         }
         return false;
@@ -325,31 +223,6 @@ Gstranslator.prototype = {
         }
     }
 };
-
-function changeAutoClose(clicked_actor) {
-    if(autoCloseVal) {
-        settings.set_string("auto-close", "0");
-        autoCloseVal = 0;
-    }
-	else {
-	    settings.set_string("auto-close", "1");
-        autoCloseVal = 1;
-	}
-}
-
-function _updateLangsLabel(text) {
-    if(text!=null){
-        langsLabel.set_text(text);
-    }
-    else {
-        langsLabel.set_text("unknown");
-    }
-}
-
-function _resetSearch() {
-    searchEntry.set_text("");
-    global.stage.set_key_focus(searchEntry);
-}
 
 function init() {
 	let GstranslatorProxy = DBus.makeProxyClass(GstranslatorInterface);
@@ -363,8 +236,26 @@ function init() {
     yoffset = parseInt(settings.get_string("yoffset"));
     autoCloseVal = parseInt(settings.get_string("auto-close"));
     height = parseInt(settings.get_string("height"));
-}
+    
+    gstrans = new Gstranslator();
+    
+    // init traslation box
+    transBox = new St.BoxLayout({style_class: "main-dialog",
+	    vertical: true
+    });
+    Main.uiGroup.add_actor(transBox);
 
+    scrollBox = new St.ScrollView();
+    transBox.add(scrollBox);
+    
+    content = new St.BoxLayout({vertical: true});
+    scrollBox.add_actor(content);
+    
+    translationLabel = new St.Label({ style_class: 'main-label'});
+    
+    // init languages info
+    langsLabel = new St.Label({ style_class: 'lang-label'});
+}
 
 function enable() {
     for(key in key_bindings) {
@@ -374,8 +265,6 @@ function enable() {
 			key_bindings[key]
 		);
 	}
-    gstrans = new Gstranslator();
-    Main.panel._rightBox.insert_child_at_index(gstrans.actor, 0);
 }
 
 function disable() {
@@ -386,14 +275,9 @@ function disable() {
     gstrans = null;
 }
 
-function _toggleMenu() {
-    gstrans.menu.toggle();
-}
-
-
-function _getPosition(text_pos) {
-    let text_height = text_pos.height;
-    if (text_pos.height>height){
+function _getPosition(labelPosition) {
+    let text_height = labelPosition.height;
+    if (labelPosition.height>height){
         text_height = height;
     }
     switch(position){
@@ -402,7 +286,7 @@ function _getPosition(text_pos) {
             y = 0+yoffset;
             break;
         case 1:
-            x = Math.floor(monitor.width-text_pos.width)-xoffset;
+            x = Math.floor(monitor.width-labelPosition.width)-xoffset;
             y = 0+yoffset;
             break;
         case 2:
@@ -410,8 +294,63 @@ function _getPosition(text_pos) {
             y = Math.floor(monitor.height-text_height)-yoffset;
             break;
         case 3:
-            x = Math.floor(monitor.width-text_pos.width)-xoffset;
+            x = Math.floor(monitor.width-labelPosition.width)-xoffset;
             y = Math.floor(monitor.height-text_height)-yoffset;
             break;   
+    }
+}
+
+// helper functions for menu
+function changeAutoClose(clicked_actor) {
+    if(autoCloseVal) {
+        settings.set_string("auto-close", "0");
+        autoCloseVal = 0;
+    }
+	else {
+	    settings.set_string("auto-close", "1");
+        autoCloseVal = 1;
+	}
+}
+
+function _toggleMenu() {
+    gstrans.menu.toggle();
+}
+
+function _resetSearch() {
+    searchEntry.set_text("");
+    global.stage.set_key_focus(searchEntry);
+}
+
+// functions responsible for handling languages
+function _updateLangsLabel(langs_string) {
+    if(langs_string!=null){
+        langsLabelMenu.set_text(langs_string);
+    }
+    else {
+        langsLabelMenu.set_text("unknown");
+    }
+}
+
+function _changeLangs(direction) {
+    dbusNotify.changeLangsRemote(direction, _showLangInfo);
+}
+
+function _showLangInfo(langs_string) {
+    if (langs_string != null){
+        langsLabel.set_text(langs_string);
+        langsLabelMenu.set_text(langs_string);
+        if (!Main.uiGroup.contains(langsLabel)){
+            Main.uiGroup.add_actor(langsLabel);
+        }
+
+        langsLabel.set_position(Math.floor(monitor.width/2), Math.floor(monitor.height/2));
+        Tweener.removeTweens(langsLabel)
+        Tweener.addTween(langsLabel, { time: 3, onComplete: _hideLangInfo });
+    }
+}
+
+function _hideLangInfo() {
+    if(Main.uiGroup.contains(langsLabel)) {
+        Main.uiGroup.remove_actor(langsLabel);
     }
 }
